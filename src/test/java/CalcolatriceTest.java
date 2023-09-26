8172551d3770c78383fcfc8a45ff15090f599b71
Giuliopalla <giulio.palladino@hotmail.it>
2023-09-26 12:01:58 +0200

    Update

---
+++ src/test/java/CalcolatriceTest.java
@@ -0,0 +1,23 @@
+import org.example.Calcolatrice;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CalcolatriceTest {
+    Calcolatrice calcolatrice;
+
+    @Before
+    public final void setUp(){
+        calcolatrice=new Calcolatrice();
+    }
+
+    @Test
+    public void testSomma(){
+        assertTrue("somma corretta",calcolatrice.somma(11,1)==12);
+    }
+    @Test
+    public void testMoltiplicazione(){
+        assertTrue("moltiplicazione corretta",calcolatrice.moltiplicazione(10,2)==20);
+    }
+}
