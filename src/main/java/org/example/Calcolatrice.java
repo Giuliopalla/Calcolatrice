13ece6bebb76b8a874b9a6a022faa115c58dfeb7
Giuliopalla <giulio.palladino@hotmail.it>
2023-09-26 11:48:41 +0200

    Esercitazione calcolatrice is

---
+++ src/main/java/org/example/Calcolatrice.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Calcolatrice {
+    
+    private int x;
+    private int y;
+    public Calcolatrice(){
+        this.x=0;
+        this.y=0;
+        
+    }
+    
+    public int somma(int x,int y){
+        return x+y;
+    }
+    public int moltiplicazione(int x,int y){
+        return x*y;
+    }
+}
